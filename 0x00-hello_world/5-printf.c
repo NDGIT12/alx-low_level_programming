13862f9f319df9104d0ba7944743372ce5c51930
ND <nada.amr1267@gmail.com>
2023-02-16 19:00:27 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,7 +2,7 @@
 
 /**
  * main - prints "with proper grammar, but the outcome is 
- *a piece of art," followed newline
+ * a piece of art," followed newline
  * Return: Always 0.
  */
 int main(void)
