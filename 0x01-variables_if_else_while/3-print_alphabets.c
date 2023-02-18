b3b23f60ddb7d6297c1225e0003965afbf7fb1cd
ND <nada.amr1267@gmail.com>
2023-02-17 17:16:58 -0800

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,9 +6,9 @@
  */
 int main(void)
 {
-char alp[26] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 int i;
-for (i = 0; i < 26; i++)
+for (i = 0; i < 52; i++)
 {
 putchar(alp[i]);
 }
