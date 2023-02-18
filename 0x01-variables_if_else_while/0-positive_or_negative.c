a4c6074d977525b6291577268e68f27058a71de3
ND <nada.amr1267@gmail.com>
2023-02-17 16:31:43 -0800

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,15 +13,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-printf("%d is positive", n);
+printf("%f is positive\n", n);
 }
 else if (n == 0)
 {
-printf("%d is zero", n);
+printf("%f is zero\n", n);
 }
 else
 {
-printf("%d is negative", n);
+printf("%f is negative\n", n);
 }
 return (0);
 }
